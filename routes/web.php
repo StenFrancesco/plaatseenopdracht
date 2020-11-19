3f577ac92524c9236f0fe5033dc62f9ef7a4ec0a
ninxi <stendert_baars@hotmail.com>
2020-11-19 14:58:39 +0100

    changed route for home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['categories' => \App\Models\Category::get_all_categories()]);
 });
 
 Auth::routes();
