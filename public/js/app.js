318067b2eba9aba82f7bac7afbdde1e7820f704b
ninxi <stendert_baars@hotmail.com>
2020-11-20 14:16:44 +0100

    image fetch2

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Github\plaatseenopdracht\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Github\plaatseenopdracht\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/stendert/Code/plaatseenopdracht/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/stendert/Code/plaatseenopdracht/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
